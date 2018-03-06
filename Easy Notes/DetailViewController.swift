55714123e1c747d4ca1a36198558f220da9c9ce9
System Administrator <root@Twahas-MacBook-Air.local>
2018-03-06 15:42:46 +0600

    Create custom class for detailsViewController

---
+++ Easy Notes/DetailViewController.swift
@@ -0,0 +1,27 @@
+//
+//  DetailViewController.swift
+//  Easy Notes
+//
+//  Created by Oceanize on 3/6/18.
+//  Copyright © 2018 webhawksit. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        textView.text = "Demo text"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
